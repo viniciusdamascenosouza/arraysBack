5f5426eefe53e5c60b7b9728a32e5fab7d7a51be
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-02 07:56:57 -0300

    Deletando elementos com pop

---
+++ deletandoElementos.js
@@ -0,0 +1,11 @@
+const notasAluno1 = [10, 6, 8, 5.5, 10];
+
+notasAluno1.pop();
+
+const mediaAluno1 = (notasAluno1[0] + notasAluno1[1] + notasAluno1[2] + notasAluno1[3]) / notasAluno1.length;
+
+console.log(`A média do Aluno 1 é: ${mediaAluno1}.`)
+
+console.log(mediaAluno1);
+
+console.log(notasAluno1);

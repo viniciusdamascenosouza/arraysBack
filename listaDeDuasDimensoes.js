5a0a13ea9af583849b6126966780dd89079e68c8
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-02 17:28:22 -0300

    lista de duas dimensões

---
+++ listaDeDuasDimensoes.js
@@ -0,0 +1,19 @@
+const alunos = [
+    'João',
+    'Juliana',
+    'Ana',
+    'Caio',
+];
+
+const medias = [
+    10,
+    8,
+    7.5,
+    9,
+];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`
+O aluno(a) ${listaDeAlunosEMedias[0][3]} tem a média de ${listaDeAlunosEMedias[1][3]}.
+`);

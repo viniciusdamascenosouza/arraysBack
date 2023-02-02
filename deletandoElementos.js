9cfc9851116a4b4ae7ec2005a7601e51831a9ee2
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-02 08:21:53 -0300

    dividindo lista com mètodo slice

--- deletandoElementos.js
+++ deletandoElementos.js
@@ -2,9 +2,11 @@ const notasAluno1 = [10, 6, 8, 5.5, 10];
 
 notasAluno1.pop();
 
-const mediaAluno1 = (notasAluno1[0] + notasAluno1[1] + notasAluno1[2] + notasAluno1[3]) / notasAluno1.length;
+const mediaAluno1 =
+  (notasAluno1[0] + notasAluno1[1] + notasAluno1[2] + notasAluno1[3]) /
+  notasAluno1.length;
 
-console.log(`A média do Aluno 1 é: ${mediaAluno1}.`)
+console.log(`A média do Aluno 1 é: ${mediaAluno1}.`);
 
 console.log(mediaAluno1);
 
---
+++ dividindoSala.js
@@ -0,0 +1,33 @@
+const alunos = [
+  "João",
+  "Juliana",
+  "Ana",
+  "Caio",
+  "Lara",
+  "Marjorie",
+  "Guilherme",
+  "Aline",
+  "Fabiana",
+  "Andre",
+  "Carlos",
+  "Paulo",
+  "Bia",
+  "Vivian",
+  "Isabela",
+  "Vinícius",
+  "Renan",
+  "Renata",
+  "Daisy",
+  "Camilo",
+];
+
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2)
+
+console.log(
+    `Os alunos da sala 1 são: ${sala1}.`
+);
+
+console.log(
+    `Os alunos da sala 2 são: ${sala2}.`
+);

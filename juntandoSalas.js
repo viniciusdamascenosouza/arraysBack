49163a4fae69c33d978e3bfe28c591195b9f9a19
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-02 17:13:35 -0300

    Concatenando arrays

---
+++ juntandoSalas.js
@@ -0,0 +1,5 @@
+const salaJS = ["Evaldo", "Camila", "Mariana"];
+const salaPython = ["Marcelo", "Luciana", "Júlio"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+console.log(salasUnificadas);

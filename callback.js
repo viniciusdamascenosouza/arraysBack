1eda7955cb2484084efa10e24037a06210d5d2b8
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-06 08:29:57 -0300

    Quando usar callback

--- callback.js
+++ callback.js
@@ -14,3 +14,14 @@ function imprimeNome(nome) {
 
 nomes.forEach(imprimeNome);
 
+
+// QUANDO USAR CADA FERRAMENTA (forEach, for of, for):
+
+// forEach() : para trabalhar com arrays da forma mais corriqueira - percorrendo do primeiro ao último elemento, 
+// sem alterar a condição de parada, e também para manter a coesão do estilo quando em conjunto com outros 
+// métodos de array como map, filter e etc;
+
+// for…of : em caso de iteráveis (dicionários, conjuntos e outras estruturas de dados) ou de arrays quando for necessário 
+// o uso de programação assíncrona e/ou dar condições de saída do laço (por exemplo, com o uso de break);
+
+// for : para casos em que seja necessário manipular de forma mais fina as fases do laço (condição inicial, condição de parada e incremento).

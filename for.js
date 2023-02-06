2097751f0dc46b7c0dd606505f146ef52fe93f1a
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-06 07:05:36 -0300

    Laços de repetição com For clássico

---
+++ for.js
@@ -0,0 +1,11 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+
+// sintaxe do For consiste em três expressões dentro do parenteses!
+
+// 1° expressão é executada uma única vez;
+// 2° expressão é a condição de execução;
+// 3° expressão é executada sempre ao final do bloco;
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
+}
